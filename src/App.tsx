0d3022659ba731c247e0ccc41dfb098f967ed5a7
ansalim01 <ansalim14@gmail.com>
2023-07-10 22:36:24 +0300

    ttt

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 import Main from "./Main";
 import { HashRouter, Route, Routes } from "react-router-dom";
+import Test from "./Test";
 
 function App() {
   return (
     <>
       <Routes>
         <Route index path={"/"} element={<Main />} />
+        <Route path={"/test"} element={<Test />} />
       </Routes>
     </>
   );
